42643f78c7dc40d63f57a5693dfef789c0dc27b7
bjrochie <46945213+bjrochie@users.noreply.github.com>
2019-05-11 20:47:43 -0500

    Adding addjob files

---
+++ saavyTechHelpDesk/addJobForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace saavyTechHelpDesk
+{
+    partial class addJobForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(addJobForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtlName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtmodel = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtserial = new System.Windows.Forms.TextBox();
+            this.cboequipType = new System.Windows.Forms.ComboBox();
+            this.cbobrand = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtproblem = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cbostatus = new System.Windows.Forms.ComboBox();
+            this.btnreset = new System.Windows.Forms.Button();
+            this.btnsaveJob = new System.Windows.Forms.Button();
+            this.txtphoneNum = new System.Windows.Forms.MaskedTextBox();
+            this.txtfName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(247, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Last Name";
+            // 
+            // txtlName
+            // 
+            this.txtlName.Location = new System.Drawing.Point(318, 53);
+            this.txtlName.Name = "txtlName";
+            this.txtlName.Size = new System.Drawing.Size(120, 20);
+            this.txtlName.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Phone #";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(247, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Email";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(318, 81);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(120, 20);
+            this.txtemail.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Equipment";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(247, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Brand";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(36, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Model";
+            // 
+            // txtmodel
+            // 
+            this.txtmodel.Location = new System.Drawing.Point(110, 139);
+            this.txtmodel.Name = "txtmodel";
+            this.txtmodel.Size = new System.Drawing.Size(120, 20);
+            this.txtmodel.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(247, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Serial";
+            // 
+            // txtserial
+            // 
+            this.txtserial.Location = new System.Drawing.Point(318, 139);
+            this.txtserial.Name = "txtserial";
+            this.txtserial.Size = new System.Drawing.Size(120, 20);
+            this.txtserial.TabIndex = 8;
+            // 
+            // cboequipType
+            // 
+            this.cboequipType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboequipType.FormattingEnabled = true;
+            this.cboequipType.Location = new System.Drawing.Point(110, 110);
+            this.cboequipType.Name = "cboequipType";
+            this.cboequipType.Size = new System.Drawing.Size(120, 21);
+            this.cboequipType.TabIndex = 5;
+            // 
+            // cbobrand
+            // 
+            this.cbobrand.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbobrand.FormattingEnabled = true;
+            this.cbobrand.Location = new System.Drawing.Point(318, 110);
+            this.cbobrand.Name = "cbobrand";
+            this.cbobrand.Size = new System.Drawing.Size(120, 21);
+            this.cbobrand.TabIndex = 6;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(39, 171);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 13);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Problem";
+            // 
+            // txtproblem
+            // 
+            this.txtproblem.Location = new System.Drawing.Point(110, 168);
+            this.txtproblem.Multiline = true;
+            this.txtproblem.Name = "txtproblem";
+            this.txtproblem.Size = new System.Drawing.Size(328, 117);
+            this.txtproblem.TabIndex = 9;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(39, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Status";
+            // 
+            // cbostatus
+            // 
+            this.cbostatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbostatus.FormattingEnabled = true;
+            this.cbostatus.Location = new System.Drawing.Point(110, 305);
+            this.cbostatus.Name = "cbostatus";
+            this.cbostatus.Size = new System.Drawing.Size(120, 21);
+            this.cbostatus.TabIndex = 10;
+            // 
+            // btnreset
+            // 
+            this.btnreset.Image = ((System.Drawing.Image)(resources.GetObject("btnreset.Image")));
+            this.btnreset.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnreset.Location = new System.Drawing.Point(141, 361);
+            this.btnreset.Name = "btnreset";
+            this.btnreset.Size = new System.Drawing.Size(115, 35);
+            this.btnreset.TabIndex = 11;
+            this.btnreset.Text = "Reset";
+            this.btnreset.UseVisualStyleBackColor = true;
+            this.btnreset.Click += new System.EventHandler(this.btnreset_Click);
+            // 
+            // btnsaveJob
+            // 
+            this.btnsaveJob.Image = ((System.Drawing.Image)(resources.GetObject("btnsaveJob.Image")));
+            this.btnsaveJob.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnsaveJob.Location = new System.Drawing.Point(281, 361);
+            this.btnsaveJob.Name = "btnsaveJob";
+            this.btnsaveJob.Size = new System.Drawing.Size(113, 35);
+            this.btnsaveJob.TabIndex = 12;
+            this.btnsaveJob.Text = "Save Job";
+            this.btnsaveJob.UseVisualStyleBackColor = true;
+            this.btnsaveJob.Click += new System.EventHandler(this.btnsaveJob_Click);
+            // 
+            // txtphoneNum
+            // 
+            this.txtphoneNum.Location = new System.Drawing.Point(110, 81);
+            this.txtphoneNum.Mask = "000-0000";
+            this.txtphoneNum.Name = "txtphoneNum";
+            this.txtphoneNum.Size = new System.Drawing.Size(120, 20);
+            this.txtphoneNum.TabIndex = 3;
+            // 
+            // txtfName
+            // 
+            this.txtfName.Location = new System.Drawing.Point(110, 53);
+            this.txtfName.Name = "txtfName";
+            this.txtfName.Size = new System.Drawing.Size(120, 20);
+            this.txtfName.TabIndex = 1;
+            // 
+            // addJobForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 441);
+            this.Controls.Add(this.txtphoneNum);
+            this.Controls.Add(this.btnsaveJob);
+            this.Controls.Add(this.btnreset);
+            this.Controls.Add(this.txtproblem);
+            this.Controls.Add(this.cbobrand);
+            this.Controls.Add(this.cbostatus);
+            this.Controls.Add(this.cboequipType);
+            this.Controls.Add(this.txtserial);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtlName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtmodel);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtfName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "addJobForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "saavyTech Add Job";
+            this.Load += new System.EventHandler(this.addJobForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtlName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtmodel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtserial;
+        private System.Windows.Forms.ComboBox cboequipType;
+        private System.Windows.Forms.ComboBox cbobrand;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtproblem;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox cbostatus;
+        private System.Windows.Forms.Button btnreset;
+        private System.Windows.Forms.Button btnsaveJob;
+        private System.Windows.Forms.MaskedTextBox txtphoneNum;
+        private System.Windows.Forms.TextBox txtfName;
+    }
+}
---
+++ saavyTechHelpDesk/addJobForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saavyTechHelpDesk
+{
+    public partial class addJobForm : Form
+    {
+        private saavyTechDBE contx;
+
+       
+        public addJobForm()
+        {
+            InitializeComponent();
+        }
+
+        private void addJobForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                contx = new saavyTechDBE();               
+
+                //Populate the equipment Dropdown
+                var equipment = contx.equiptmentType.ToList();
+                cboequipType.DataSource = equipment;
+                cboequipType.DisplayMember = "equiptmentName";
+                cboequipType.ValueMember = "Id";
+
+                //Populate the brand Dropdown
+                var brand = contx.Brand.ToList();
+                cbobrand.DataSource = brand;
+                cbobrand.DisplayMember = "brandName";
+                cbobrand.ValueMember = "Id";
+
+                //Populate the status Dropdown
+                var status = contx.status.ToList();
+                cbostatus.DataSource = status;
+                cbostatus.DisplayMember = "statusName";
+                cbostatus.ValueMember = "Id";
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string UppercaseFirst(string s)
+        {
+            return char.ToUpper(s[0]) + s.Substring(1);
+        }
+
+        private void btnsaveJob_Click(object sender, EventArgs e)
+        {
+            var parent = (saavyTechManinForm)MdiParent;
+            var roleId = parent.userRole;
+
+            try
+            {
+                //Collect data from the form
+                var fname = UppercaseFirst(txtfName.Text);
+                var lname = UppercaseFirst(txtlName.Text);
+                var pNumber = txtphoneNum.Text;
+                var mail = txtemail.Text;
+                var model = txtmodel.Text;
+                var serial = txtserial.Text;
+                var problem = txtproblem.Text;
+
+                var equipment = Convert.ToInt32(cboequipType.SelectedValue);
+                var brand = Convert.ToInt32(cbobrand.SelectedValue);
+                var status = Convert.ToInt32(cbostatus.SelectedValue);
+
+                //generate job number
+                char[] letters = "qwertyuiopasdfghjklzxcvbnm0123456789".ToCharArray();
+                Random l = new Random();
+                string randomString = "";
+                for (int i = 0; i < 6; i++)
+                {
+                    randomString += letters[l.Next(0, 35)].ToString();
+                }
+                var jobNumber = $"{fname[0]}{lname[0]}{randomString}";
+
+                //Validate minimum data is collected, as well as any other validation that you may want to enforce.
+                if (isFormInvalid())
+                {
+                    MessageBox.Show("Please validate all data before submission!");
+                }
+
+                //Do further validations to check if job number already exists
+                else if (CheckJob(jobNumber))
+                {
+                    MessageBox.Show("This job already exists!");
+                }
+                else
+                {
+                    var newJob = new Jobs
+                    {
+                        firstName = fname,
+                        lastName = lname,
+                        contact = pNumber,
+                        email = mail,
+                        equipmentTypeId = equipment,
+                        brandId = brand,
+                        model = model,
+                        serial = serial,
+                        issues = problem,
+                        statusId = status,
+                        recepId = roleId,
+                        dateIn = DateTime.Now,
+                        jobNumber = jobNumber                        
+                    };
+
+                    contx.Jobs.Add(newJob);
+                    contx.SaveChanges();
+                    MessageBox.Show("Job #" + jobNumber + " \nfor " + fname + " " + lname + " successfully added.");
+
+                    ResetForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"A fatal. {ex.Message}");
+            }
+        }
+
+        void ResetForm()
+        {
+            txtfName.Clear();
+            txtlName.Clear();
+            txtphoneNum.Clear();
+            txtemail.Clear();
+            txtmodel.Clear();
+            txtserial.Clear();
+            txtproblem.Clear();
+
+            cboequipType.SelectedIndex = 0;
+            cbobrand.SelectedIndex = 0;            
+            cbostatus.SelectedIndex = 0;
+        }
+
+        private void btnreset_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        bool CheckJob(string jobNumber)
+        {
+            var jobExists = contx.Jobs.Any(q => q.jobNumber.Trim() == jobNumber);
+            return jobExists;
+        }
+
+        bool isFormInvalid()
+        {
+            return String.IsNullOrEmpty(txtfName.Text) || String.IsNullOrEmpty(txtphoneNum.Text);
+        }
+    }
+}
