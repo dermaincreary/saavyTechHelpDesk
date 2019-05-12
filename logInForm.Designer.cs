d29b1f3f632e143b5d14083761e7f7090e1b6910
bjrochie <46945213+bjrochie@users.noreply.github.com>
2019-05-11 20:51:14 -0500

    Adding loginForm files

---
+++ logInForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace saavyTechHelpDesk
+{
+    partial class logInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(logInForm));
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(77, 9);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(140, 20);
+            this.txtusername.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(77, 46);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(140, 20);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(41, 86);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "&Reset";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(141, 86);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "&Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtNotify
+            // 
+            this.txtNotify.Text = "notifyIcon1";
+            this.txtNotify.Visible = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnLogin);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.txtPassword);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtusername);
+            this.panel1.Location = new System.Drawing.Point(118, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(232, 136);
+            this.panel1.TabIndex = 7;
+            // 
+            // logInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 181);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "logInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "saavyTech Login";
+            this.Load += new System.EventHandler(this.logInForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.NotifyIcon txtNotify;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ logInForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace saavyTechHelpDesk
+{
+    public partial class logInForm : Form
+    {
+        public logInForm()
+        {
+            InitializeComponent();
+        }
+
+        private void logInForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtusername.Clear();
+            txtPassword.Clear();
+            txtusername.Focus();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var username = txtusername.Text;
+            var password = txtPassword.Text;
+
+            //declare object of DB model
+            var contx = new saavyTechDBE();
+
+            //check if true value was returned fron the user check and grant access
+            //If false, then continue to restrict access
+            var user = contx.Users.SingleOrDefault(q => q.username == username && q.password == password);
+
+            if (user != null)
+            {
+                var parent = (saavyTechManinForm)MdiParent;
+                parent.isLoggedIn = true;
+
+                var usr = user.firstName + " " + user.lastName;
+
+                var roleID = user.roleId;
+                int userid = user.Id;                
+
+                parent.userName = usr;
+                parent.userRole = (int)roleID;
+                parent.userId = userid;
+
+                parent.checkjobs();
+                parent.UpdateMain();
+
+                //notify icon
+                txtNotify.Text = "User " + username + "is logged in";
+                txtNotify.BalloonTipText = "User " + usr + " is logged in";
+                txtNotify.BalloonTipTitle = "Login Successful";
+                txtNotify.Icon = SystemIcons.Application;
+                txtNotify.ShowBalloonTip(1000);
+                this.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("Invalid Credentials. Please try again.");
+                txtusername.Text = "";
+                txtPassword.Text = "";
+                txtusername.Focus();
+            }
+        }
+    }
+}
